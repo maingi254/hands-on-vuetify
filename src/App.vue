eaae0e31cd7bb3f228fea8fec372a076b2a775a2
maingi254 <itskelvinmaingi@gmail.com>
2020-01-23 18:04:50 +0300

    working on stepper component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar
+    app
+    color="primary"
+    >
+    </v-app-bar>
+    <v-content>
+      <checkout/>
+    </v-content>
+    
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import checkout from './components/checkout'
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+  name:'App',
+components:{
+  checkout
+}
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/checkout.vue
@@ -0,0 +1,57 @@
+<template>
+    <v-stepper
+    v-model="currentStep">
+        <v-stepper-header>
+            <v-stepper-step :complete="currentStep > 1" step="1">Shipping</v-stepper-step>
+            <v-divider></v-divider>
+            <v-stepper-step :complete="currentStep > 2" step="2">payment</v-stepper-step>
+            <v-divider></v-divider>
+            <v-stepper-step step="3">place Order</v-stepper-step>
+        </v-stepper-header>
+        <v-stepper-items>
+                <v-stepper-content step="1">
+                    <v-card>
+                        <h1>shipping step</h1>
+                    </v-card>
+                    <v-btn
+                    color="primary"
+                    @click="currentStep = 2">
+                    continue
+                    </v-btn>
+                </v-stepper-content>
+                <v-stepper-content step="2">
+                    <v-card>
+                        <h1>Payment step</h1>
+                    </v-card>
+                    <v-btn
+                    color="primary"
+                    @click="currentStep = 3">
+                    continue
+                    </v-btn>
+                </v-stepper-content>
+                <v-stepper-content step="3">
+                    <v-card>
+                        <h1>Place order step</h1>
+                    </v-card>
+                    <v-btn
+                    color="primary"
+                    @click="currentStep = 1">
+                    continue
+                    </v-btn>
+                </v-stepper-content>
+            </v-stepper-items>
+    </v-stepper>
+</template>
+
+<script>
+export default {
+name:'checkout',
+data : () =>({
+    currentStep:0
+}),
+}
+</script>
+
+<style>
+
+</style>
